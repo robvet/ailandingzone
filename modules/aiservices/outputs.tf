f10e8cfab47dfe81ff530977d9e80f9cd2de6dbe
Sree Ram <sreekanth_bhr@yahoo.com>
2025-02-16 19:39:03 -0600

    Update outputs.tf

--- modules/aiservices/outputs.tf
+++ modules/aiservices/outputs.tf
@@ -1,12 +1,16 @@
 output "key" {
-  value     = azurerm_cognitive_account.this.primary_access_key
+  value     = azurerm_ai_services.this.primary_access_key
   sensitive = true
 }
 
 output "name" {
-  value = azurerm_cognitive_account.this.name
+  value = azurerm_ai_services.this.name
 }
 
 output "id" {
-  value = azurerm_cognitive_account.this.id
+  value = azurerm_ai_services.this.id
+}
+
+output "AIServicesResource" {
+  value = azurerm_ai_services.this.endpoint
 }
