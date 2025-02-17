f3c76a61b574eef02c7b7367da1d7d038accf349
Sree Ram <sreekanth_bhr@yahoo.com>
2025-02-16 19:37:23 -0600

    Update main.tf

--- modules/pe/main.tf
+++ modules/pe/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "~> 3.80"
+      version               = "4.19.0"
       configuration_aliases = [azurerm.platform, azurerm.analytics]
     }
   }
